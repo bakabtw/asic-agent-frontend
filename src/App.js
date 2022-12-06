57d6ddbdda1d6b7a208203191691d6bb3401cf76
Eugene <me@knst.me>
2022-12-06 17:08:29 +0300

    Added initial support for Submit button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button, Grid, Header, Image, Message, Form, Segment } from 'semantic-ui-react'
+import { Button, Grid, Header, Image, Message, Form, Segment, GridColumn, GridRow } from 'semantic-ui-react'
 
 class App extends React.Component {
   constructor(props) {
@@ -11,7 +11,7 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    // this.fetchPowerData()
+    this.fetchPowerData()
 
     this.timerID = setInterval(
       () => this.fetchPowerData(), 5000
@@ -24,20 +24,30 @@ class App extends React.Component {
 
   fetchPowerData = () => {
     fetch('https://power.knst.me/get_power')
-    .then(response => response.json())
-    .then((data) => {
+      .then(response => response.json())
+      .then((data) => {
 
-      console.log(data);
-      this.setState({available_power: data.power});
+        console.log(data);
+        this.setState({ available_power: data.power });
 
-    })
-    .catch(function(error) {
-      console.log(error);
-    });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  setPowerData = () => {}
+
+  handleUpdateButton = () => {
+    this.fetchPowerData()
+  }
+
+  handleSubmitButton = () => {
+    this.setPowerData()
   }
 
   render() {
-    return(
+    return (
       <Grid textAlign='center' style={{ padding: '10em 5em 5em 5em' }}>
         <Grid.Column style={{ maxWidth: 450 }}>
           <Header as='h2' color='teal' textAlign='center'>
@@ -46,13 +56,24 @@ class App extends React.Component {
           <Form>
             <Segment stacked>
               <Form.Input fluid icon='power' iconPosition='left' placeholder='Available power (W)' />
-              <Button onClick={this.fetchPowerData} fluid color='teal' size='large'>
-                Update
-              </Button>
+              <Grid divided='vertically'>
+                <GridRow columns={2}>
+                  <Grid.Column>
+                    <Button onClick={this.handleSubmitButton} fluid color='teal' size='large'>
+                      Submit
+                    </Button>
+                  </Grid.Column>
+                  <GridColumn>
+                    <Button onClick={this.handleUpdateButton} fluid color='grey' size='large'>
+                      Update
+                    </Button>
+                  </GridColumn>
+                </GridRow>
+              </Grid>
             </Segment>
           </Form>
           <Message>
-            Available power: {this.state.available_power}W
+            Available power: {this.state.available_power == 0 ? 'Updating...' : this.state.available_power + 'W'}
           </Message>
         </Grid.Column>
       </Grid>
