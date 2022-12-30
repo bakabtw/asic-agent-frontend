501735f11762dfca5a68e0783698fc3aa787a0f2
Eugene Konstantinov <git@knst.me>
2022-12-30 19:22:18 +0300

    Added defaultValue

--- src/Components/AsicCrudComponent.js
+++ src/Components/AsicCrudComponent.js
@@ -3,7 +3,7 @@ import { Grid, Header, Image, Form, Segment } from 'semantic-ui-react';
 import AppMessages from './AppMessages';
 
 const AsicCrudComponent = ({ asicData, apiHost, addMessage, deleteMessage, messageQueue }) => {
-  const [formData, setFormData] = useState(asicData);
+  const [formData, setFormData] = useState({});
   const [loadingForm, setLoadingForm] = useState(false);
 
   const handleSubmitButton = () => {
@@ -54,19 +54,20 @@ const AsicCrudComponent = ({ asicData, apiHost, addMessage, deleteMessage, messa
         </Header>
         <Segment stacked>
           <Form loading={loadingForm}>
-            <Form.Input name='id' value={formData['id']} fluid type='hidden' />
+            <Form.Input name='id' defaultValue={asicData['id']} fluid type='hidden' />
             <Form.Group widths='equal'>
-              <Form.Input onChange={handleInputChange} name='ip' fluid label='IP' placeholder='ASIC API IP' />
-              <Form.Input onChange={handleInputChange} name='port' fluid label='Port' placeholder='ASIC API port' />
+              <Form.Input onChange={handleInputChange} name='ip' defaultValue={asicData['ip']} fluid label='IP' placeholder='ASIC API IP' />
+              <Form.Input onChange={handleInputChange} name='port' defaultValue={asicData['port']} fluid label='Port' placeholder='ASIC API port' />
             </Form.Group>
             <Form.Group widths='equal'>
-              <Form.Input onChange={handleInputChange} name='username' fluid label='User' placeholder='User' />
-              <Form.Input onChange={handleInputChange} name='password' fluid label='Password' placeholder='Password' />
+              <Form.Input onChange={handleInputChange} name='user' defaultValue={asicData['user']} fluid label='User' placeholder='User' />
+              <Form.Input onChange={handleInputChange} name='password' defaultValue={asicData['password']} fluid label='Password' placeholder='Password' />
             </Form.Group>
             <Form.Group widths='equal'>
               <Form.Select
                 onChange={(e, data) => handleSelectChange(e, data)}
                 name='type'
+                value={asicData['type']}
                 fluid
                 label='ASIC model'
                 options={[
@@ -74,16 +75,17 @@ const AsicCrudComponent = ({ asicData, apiHost, addMessage, deleteMessage, messa
                 ]}
                 placeholder='ASIC model'
               />
-              <Form.Input onChange={handleInputChange} name='power' fluid type='number' label='Power' placeholder='Max power (W)' />
+              <Form.Input onChange={handleInputChange} name='power' defaultValue={asicData['power']} fluid type='number' label='Power' placeholder='Max power (W)' />
               <Form.Select
                 onChange={(e, data) => handleSelectChange(e, data)}
                 name='phase'
+                value={asicData['phase']}
                 fluid
                 label='Phase'
                 options={[
-                  { key: 'phaseA', text: 'A', value: 'a' },
-                  { key: 'phaseB', text: 'B', value: 'b' },
-                  { key: 'phaseC', text: 'C', value: 'c' },
+                  { key: 'phaseA', text: 'A', value: 'A' },
+                  { key: 'phaseB', text: 'B', value: 'B' },
+                  { key: 'phaseC', text: 'C', value: 'C' },
                 ]}
                 placeholder='Phase'
               />
